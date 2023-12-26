f31caa9ab45f2fae5a0be21c5931feab2409f0c1
Yuanhao <min.yuanhao.1997.11.12@outlook.com>
2023-12-26 18:18:04 +0100

    adjust layout and add clock image

--- app/src/main/java/com/yuanhao/dota2quiz/MainActivity.java
+++ app/src/main/java/com/yuanhao/dota2quiz/MainActivity.java
@@ -108,7 +108,6 @@ public class MainActivity extends AppCompatActivity {
             }
 
             public void onFinish() {
-                countDownTimerText.setText("时间到!");
             }
         }.start();
     }
